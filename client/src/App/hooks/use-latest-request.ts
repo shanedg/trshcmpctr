6fa74f69322eea6f80363ff9115b12bf358a6839
Shane Garrity <shanedgarrity@gmail.com>
2026-01-07 02:28:50 -0500

    fix(client): avoid unnecessary rerenders with use-latest-request

--- client/src/App/hooks/use-latest-request.ts
+++ client/src/App/hooks/use-latest-request.ts
@@ -2,9 +2,14 @@ import axios, { AxiosResponse, isCancel, type AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
 
 interface UseLatestRequestReturnType<T> {
-  data: T | null,
-  error: Error | null,
-  isLoading: boolean,
+  data: T | null;
+  error: Error | null;
+  isLoading: boolean;
+}
+
+interface UseLatestRequestProps<D> {
+  config?: AxiosRequestConfig<D>;
+  url: string;
 }
 
 /**
@@ -14,48 +19,48 @@ interface UseLatestRequestReturnType<T> {
  * @param config Request configuration
  * @returns A hook for wrapping network requests in a standard interface
  */
-export const useLatestRequest = <T, D = never>(url: string, config?: AxiosRequestConfig<D>) => {
-  const useRequestWithCancel = (): UseLatestRequestReturnType<T> => {
-    const [data, setData] = useState<T | null>(null);
-    const [error, setError] = useState<Error | null>(null);
-    const [isLoading, setIsLoading] = useState<boolean>(true);
+export const useLatestRequest = <T, D = never>({
+  config,
+  url,
+}: UseLatestRequestProps<D>): UseLatestRequestReturnType<T> => {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-    useEffect(() => {
-      const controller = new AbortController();
+  useEffect(() => {
+    const controller = new AbortController();
 
-      if (data === null && error === null) {
-        axios.request<T>({
-          signal: controller.signal,
-          url,
-          ...(config ?? {})
+    if (data === null && error === null) {
+      axios.request<T>({
+        signal: controller.signal,
+        url,
+        ...(config ?? {})
+      })
+        .then((response: AxiosResponse<T>) => {
+          setData(response.data);
+          setIsLoading(false);
         })
-          .then((response: AxiosResponse<T>) => {
-            setData(response.data);
+        .catch((error: unknown) => {
+          // Aborting a request throws an error that should be ignored
+          // https://axios-http.com/docs/cancellation
+          // Canceled, stale requests do not change the loading state
+          // because either:
+          // 1. the hook was unmounted before the request completed; or
+          // 2. a more recent request is now in-flight
+          if (!isCancel(error)) {
+            setError(error as Error);
             setIsLoading(false);
-          })
-          .catch((error: unknown) => {
-            // Aborting a request throws an error that should be ignored
-            // https://axios-http.com/docs/cancellation
-            // Canceled, stale requests do not change the loading state
-            // because either:
-            // 1. the hook was unmounted before the request completed; or
-            // 2. a more recent request is now in-flight
-            if (!isCancel(error)) {
-              setError(error as Error);
-              setIsLoading(false);
-            }
-          });
-      }
+          }
+        });
+    }
 
-      // Cancel stale in-flight requests when cleaned up
-      return () => { controller.abort(); };
-    }, [data, error]);
+    // Cancel stale in-flight requests when cleaned up
+    return () => { controller.abort(); };
+  }, [config, data, error, url]);
 
-    return {
-      data,
-      error,
-      isLoading,
-    };
+  return {
+    data,
+    error,
+    isLoading,
   };
-  return useRequestWithCancel;
 };
