95b961368239d5f142b71b25b19cc3374244dea5
Shane Garrity <shanedgarrity@gmail.com>
2026-07-08 04:14:35 -0400

    build(scaffold): avoid duplicating plop library version
    Make plop a regular dependency instead of a peer.

--- projects/scaffold/src/index.js
+++ projects/scaffold/src/index.js
@@ -1,59 +1,14 @@
-import { join } from 'node:path';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-const relativePathToRepoRoot = '../../';
+import { Plop, run } from 'plop';
 
-/**
- * Plopfile
- * See https://plopjs.com/documentation/
- * @param plop plop API object
- */
-export default function (plop) {
-  plop.setGenerator('project', {
-    description: 'scaffold a new js project',
-
-    // inquirer prompts
-    // https://www.npmjs.com/package/inquirer
-    prompts: [
-      {
-        type: 'input',
-        name: 'scope',
-        message: 'project scope please (omit @-prefix, <enter> for "trshcmpctr")',
-        default: 'trshcmpctr'
-      },
-      {
-        type: 'input',
-        name: 'name',
-        message: 'project name please',
-      },
-      {
-        type: 'input',
-        name: 'path',
-        message: 'project path please (omit "/projects")',
-      },
-    ],
-
-    actions: [
-      {
-        type: 'addMany',
-        destination: `${relativePathToRepoRoot}{{path}}/`,
-        base: 'templates',
-        templateFiles: '**/*.hbs',
-        // By default, globs don't match file names that start with dot, i.e. '.eslintrc.js.hbs'.
-        globOptions: { dot: true },
-      },
-
-      // Update rush.json projects
-      {
-        type: 'append',
-        path: join(relativePathToRepoRoot, '..', 'rush.json'),
-        // This regular expression matches the start of the projects list.
-        // The template is appended immediately after this line.
-        pattern: /"projects": \[/,
-        template: `    {
-      "packageName": "@{{scope}}/{{name}}",
-      "projectFolder": "{{path}}"
-    },`
-      },
-    ]
-  });
-}
+Plop.prepare(
+  {
+    configPath: join(
+      dirname(fileURLToPath(import.meta.url)),
+      'plopfile.js',
+    ),
+  },
+  env => Plop.execute(env, run)
+);
